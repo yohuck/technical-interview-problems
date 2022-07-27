668fc8b39cb6a7cd1e9d3ae9af8b5dc3a3206957
Eric Lake <ericallenlake@gmail.com>
2022-07-26 18:05:21 -0600

    Solves odd triangle

---
+++ odd-triangle.js
@@ -0,0 +1,21 @@
+let oddRow = (n) => {
+    let arr = []
+    let firstNum = (n*(n-1) + 1);
+    for (let i = 0; i < n; i++){
+        let j = firstNum + (i*2);
+        arr.push(j)
+    } return arr
+}
+
+console.log(oddRow(4))
+console.log(oddRow(12))
+
+let sumOddRow = n => {
+    let sum = 0;
+    let firstNum = (n*(n-1) + 1)
+    for (let i = 0; i < n; i++){
+        sum += (firstNum + (i*2))
+    } return sum
+}
+
+console.log(sumOddRow(12))
