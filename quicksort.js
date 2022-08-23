354bd50aff24e1e1f7faf8c6a088c2da58431ba5
Eric Lake <ericallenlake@gmail.com>
2022-08-23 13:41:16 -0600

    Quicksort 2 different ways

--- quicksort.js
+++ quicksort.js
@@ -1,5 +1,5 @@
 //initialize unsorted array
-let unsorted = [65,88,41,23,50,90,34,2,17,77]
+let unsorted = [65,88,41,23,50,24,22,34,2,17,77]
 
 const quickSort = arr => {
     // Stop condition for recursion
@@ -24,13 +24,60 @@ const quickSort = arr => {
     let bottom = quickSort(lower)
     // Recursively sort the higher section
     let top = quickSort(higher)
-
     // Add the pivot to the bottom
     bottom.push(temp)
 
-    // Concat wit the top and return
+    // Concat with the top and return
     return bottom.concat(top)
 
 }
 
-console.log(quickSort(unsorted)) // [2, 17, 23, 34, 41, 50, 65, 77, 88, 90]
+console.log(quickSort(unsorted))
+
+
+const quickSort2 = arr => {
+    // Stop condition for recursion
+    if (arr.length <= 1){
+        return arr
+    } 
+    // Find the pivot 
+    let pivot = Math.floor((arr.length/2))
+    // Swap the pivot with the last element
+    let temp = arr[pivot]
+    arr[pivot] = arr[(arr.length - 1)]
+    arr[(arr.length - 1)] = temp
+    // Set left and right pointers
+    let left = 0
+    let right = (arr.length - 1)
+    // Move the pointers until they match & swap when left is higher and right is lower
+    while (arr[left] != arr[right]){
+        let tempy
+        if (arr[left] > arr[(arr.length - 1)]){
+            tempy = arr[left]
+            if (arr[right] < arr[(arr.length - 1)]){
+                arr[left] = arr[right]
+                arr[right] = tempy
+                right--
+                left ++
+            } else {
+                right--
+            }
+        } else{
+            left++
+        }
+    }
+
+    let bottom = []
+    let returnArr = []
+
+    for (let i = 0; i < left ; i++){
+        bottom.push(arr.shift())
+    }
+
+    let middle = arr.pop()
+    return returnArr.concat(quickSort2(bottom), middle, quickSort2(arr))
+
+
+}
+
+console.log(quickSort2(unsorted))
