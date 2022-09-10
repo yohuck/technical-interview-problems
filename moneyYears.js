39e2117453a62131271045b25c4608f7df6ff546
Yo Huck <ericallenlake@gmail.com>
2022-09-10 10:13:13 -0700

    Solves moneyYears

--- moneyYears.js
+++ moneyYears.js
@@ -1,26 +1,14 @@
-const calculateYears = (p, i, t, d) => {
-    let y = 0
-    console.log(p,i,t,d,y)
-    const cY = (p,i,t,d,y) => {
-
-        console.log(p, d, y)
-        if (p > d){
-          console.log(y)
-          return y
-        } else {
-            let year = y += 1
-            console.log(year)
-            let gross = p + p*i
-            let tax = (gross - p)*t
-            let net = gross - tax
-            cY(net, i, t, d, year)
-        }
-
-
+const calculateYears = (p, i, t, d, y=0) => {
+    if (p >= d){
+      return y
+    } else {
+        let year = y += 1
+        let gross = p + p*i
+        let tax = (gross - p)*t
+        let net = gross - tax
+        return calculateYears(net, i, t, d, year)
     }
-    cY(p,i,t,d,y)
-    
-  }
+}
 
   const test = [1000, 0.05, 0.18, 1100]
 
