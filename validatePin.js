0598c05f182f85bdc05aa0f1be7d9363d049c54c
Eric Lake <ericallenlake@gmail.com>
2022-10-20 00:00:31 -0600

    Better regex

--- validatePin.js
+++ validatePin.js
@@ -1,7 +1,6 @@
 const validatePIN = pin => {
-    const four = /^\d{4}$/g
-    const six = /^\d{6}$/g
-    return four.test(pin) || six.test(pin)
+    const four = /^\d{4}$|^\d{6}/g
+    return four.test(pin)
 }
 
 console.log(validatePIN('123344'))
